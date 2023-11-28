a8a326e0ffa25add78f2dfa29214f01603955cab
Eleonora <ezhyrlitsyna@gmail.com>
2023-11-28 20:07:48 +0200

    add alias

--- music/src/main.js
+++ music/src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import VeeValidate from '@/includes/validation'
-import i18n from './includes/i18n'
+import i18n from '@/includes/i18n'
 
 import './assets/base.css'
 import './assets/main.css'
